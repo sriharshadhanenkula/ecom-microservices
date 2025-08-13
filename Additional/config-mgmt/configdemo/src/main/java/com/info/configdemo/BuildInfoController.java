ac1cf61a7903a093bae9b53864522b07be49833b
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-08-13 12:24:50 -0400

    implemented interservice Communication Project Setup Used RestTemplate for communication

--- Additional/config-mgmt/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
+++ Additional/config-mgmt/configdemo/src/main/java/com/info/configdemo/BuildInfoController.java
@@ -2,11 +2,13 @@ package com.info.configdemo;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
 
 //    @Value("${build.id:default}")
---
+++ Additional/interservice/consumer/src/main/java/com/demo/consumer/restTemplate/RestTemplateClient.java
@@ -0,0 +1,18 @@
+package com.demo.consumer.restTemplate;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@AllArgsConstructor
+public class RestTemplateClient {
+
+    private final RestTemplate restTemplate;
+
+    private static final String PROVIDER_URL = "http://localhost:8081";
+
+    public String getInstanceInfo(){
+        return restTemplate.getForObject(PROVIDER_URL +"/instance-info", String.class);
+    }
+}
