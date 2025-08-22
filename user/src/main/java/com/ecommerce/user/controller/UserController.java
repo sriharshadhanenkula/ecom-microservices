6b8f499778881efd1af95bc2f6604f2e6ff9e1d9
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-08-22 14:36:31 -0400

    Using SL4J for Logging

--- user/src/main/java/com/ecommerce/user/controller/UserController.java
+++ user/src/main/java/com/ecommerce/user/controller/UserController.java
@@ -5,6 +5,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +17,11 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     private final UserService userService;
+//    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAllUsers(){
@@ -26,7 +31,23 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id){
 
-       return userService.fetchUser(id)
+//        logger.info("request received for user: {}",id);
+//
+//        logger.trace("This is trace level - very detailed logs");
+//        logger.debug("This is Debug level - used for development debugging");
+//        logger.info("This is INFO level -General System information");
+//        logger.warn("This is Warn level - Something might be wrong");
+//        logger.error("This is Error level - Something filed");
+
+        log.info("request received for user: {}",id);
+
+        log.trace("This is trace level - very detailed logs");
+        log.debug("This is Debug level - used for development debugging");
+        log.info("This is INFO level -General System information");
+        log.warn("This is Warn level - Something might be wrong");
+        log.error("This is Error level - Something filed");
+
+        return userService.fetchUser(id)
                .map(ResponseEntity::ok)
                .orElseGet(() -> ResponseEntity.notFound().build());
     }
