385dce1ecfb041f17b235dd58ca141f39e5688e3
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-09-04 11:54:10 -0400

    Redis RateLimiter Spring Cloud Gateway

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -1,22 +1,41 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter(){
+        return new RedisRateLimiter(10,20,1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName());
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("product-service", r-> r
-                        .path("/api/products/**")
-                        .filters(f-> f.retry(retryConfig -> retryConfig
+                            .path("/api/products/**")
+                            .filters(f-> f.retry(retryConfig -> retryConfig
                                                 .setRetries(10)
                                                 .setMethods(HttpMethod.GET)
+                                        )
+                                .requestRateLimiter(config -> config
+                                                .setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(hostNameKeyResolver())
                                         )
                                 .circuitBreaker(config -> config
                                 .setName("ecomBreaker")
