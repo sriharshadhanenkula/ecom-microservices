918e719c00fe9fb0742cfc2947f484c54409882f
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-09-08 09:42:09 -0400

    Setting Up Kafka Producer

---
+++ Additional/kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.demo.producer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class KafkaProducer {
+
+    private final KafkaTemplate<String,String> kafkaTemplate;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message){
+        kafkaTemplate.send("my-topic", message);
+        return "message Sent: "+ message;
+    }
+}
