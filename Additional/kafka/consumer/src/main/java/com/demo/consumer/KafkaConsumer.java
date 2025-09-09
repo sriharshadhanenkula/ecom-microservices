887469057fa64d866ec5a15c7beba96851babf13
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-09-09 13:03:59 -0400

    Setting up Kafka Producer and Multiple Kafka Consumers

---
+++ Additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -0,0 +1,18 @@
+package com.demo.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen1(String message){
+        System.out.println("Received message1:" + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen2(String message){
+        System.out.println("Received message2:" + message);
+    }
+}
--- Additional/kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
+++ Additional/kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
@@ -1,6 +1,6 @@
 package com.demo.producer;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class KafkaProducer {
 
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message){
-        kafkaTemplate.send("my-topic", message);
+        kafkaTemplate.send("my-topic-new", message);
         return "message Sent: "+ message;
     }
 }
