20c429ed6c529b0411b5f5e701735171527edd7c
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2025-09-27 14:25:51 -0400

    Kafka topics, consumer groups, partitons with cloud streams

---
+++ Additional/kafka/consumer/src/main/java/com/demo/consumer/KafkaConsumerNew.java
@@ -0,0 +1,26 @@
+package com.demo.consumer;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class KafkaConsumerNew {
+
+    @Bean
+    public Consumer<RiderLocation> processRiderLocation(){
+
+        return location -> {
+            System.out.println("Received: " + location.getRiderId());
+        };
+    }
+
+    @Bean
+    public Consumer<String> processRiderStatus() {
+        return status -> {
+            System.out.println("Received: " + status);
+        };
+    }
+}
--- Additional/kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
+++ Additional/kafka/producer/src/main/java/com/demo/producer/KafkaProducer.java
@@ -1,25 +1,25 @@
-package com.demo.producer;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-@RequiredArgsConstructor
-public class KafkaProducer {
-
-    private final KafkaTemplate<String,RiderLocation> kafkaTemplate;
-
-    @PostMapping("/send")
-    public String sendMessage(@RequestParam String message){
-
-        RiderLocation location = new RiderLocation("rider123",12.3,56.6);
-
-        kafkaTemplate.send("my-topic-new",location );
-        return "message Sent: "+ location.getRiderId();
-    }
-}
+//package com.demo.producer;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//@RequiredArgsConstructor
+//public class KafkaProducer {
+//
+//    private final KafkaTemplate<String,RiderLocation> kafkaTemplate;
+//
+//    @PostMapping("/send")
+//    public String sendMessage(@RequestParam String message){
+//
+//        RiderLocation location = new RiderLocation("rider123",12.3,56.6);
+//
+//        kafkaTemplate.send("my-topic-new",location );
+//        return "message Sent: "+ location.getRiderId();
+//    }
+//}
